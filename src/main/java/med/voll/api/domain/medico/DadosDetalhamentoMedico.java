9d290b07bbea6c9674cf3ee2007f9b3302acc8fe
Fabíola <fabiolasoufer@gmail.com>
2023-11-27 07:36:28 -0300

    @fabiolasoufer Agendamento de consultas

--- src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
@@ -3,7 +3,7 @@ package med.voll.api.domain.medico;
 import med.voll.api.domain.endereco.Endereco;
 
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
-    public DadosDetalhamentoMedico ( Medico medico) {
+    public DadosDetalhamentoMedico (Medico medico) {
         this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
     }
 }
